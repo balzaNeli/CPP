5cef7f20bc5e1e6759de21ad620a1a8c22574f10
Cauã Balzaneli <cauabalzaneli81@gmail.com>
2026-04-25 19:26:30 -0300

    add verificadorPalindromo.cpp

--- verificadorPalindromo.cpp
+++ verificadorPalindromo.cpp
@@ -4,13 +4,30 @@
    Finalidade: Leia uma string e verifique se ela é um palíndromo (igual lida de trás para frente).
 */
 
-
 #include <iostream>
+#include <string>
 using namespace std;
 
-
-
 int main() {
-    cout << "Hello, World!" << endl;
+    string palavra;
+    cout << "Digite uma palavra: ";
+    cin >> palavra;
+
+    int tamanho = palavra.size(); // quantas letras tem
+    bool palindromo = true;       // assume que é palíndromo até provar o contrário
+
+    for (int i = 0; i < tamanho / 2; i++) {
+        // compara letra da frente com letra de trás
+        if (palavra[i] != palavra[tamanho - 1 - i]) {
+            palindromo = false; // achou diferença, não é palíndromo
+            break;              // não precisa continuar comparando
+        }
+    }
+
+    if (palindromo)
+        cout << palavra << " é um palindromo!" << endl;
+    else
+        cout << palavra << " nao é um palindromo." << endl;
+
     return 0;
 }
